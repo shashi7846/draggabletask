d0bca9e67c6700321d56f9ccea88ea501a107156
shashi7846 <shashikanth7846@gmail.com>
2021-07-08 14:31:41 +0530

    commit

--- src/axios.js
+++ src/axios.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export function Getuser(){
-    return axios.get("http://localhost:3000/user")
+    return axios.get("https://manageusers-be.herokuapp.com/user")
 }
 
 export function Getuserbyid(id){
-    return axios.get(`http://localhost:3000/user/${id}`);
+    return axios.get(`https://manageusers-be.herokuapp.com/user/${id}`);
 }
 
 export function Postuser(data){
-    return axios.post("http://localhost:3000/user",data)
+    return axios.post("https://manageusers-be.herokuapp.com/user",data)
 }
 
 export function Deleteuserbyid(id){
-    return axios.delete(`http://localhost:3000/user/${id}`);
+    return axios.delete(`https://manageusers-be.herokuapp.com/user/${id}`);
 }
 
 export function Updateuserbyid(id,data){
-    return axios.put(`http://localhost:3000/user/${id}`,data)
+    return axios.put(`https://manageusers-be.herokuapp.com/user/${id}`,data)
 }
